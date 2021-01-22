86d10f32c92064e480af90fc27d96a5b579494df
unknown <becklabash@gmail.com>
2021-01-22 15:52:01 -0500

    add new scripts

--- ttk.py
+++ ttk.py
@@ -1,47 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 31 19:38:35 2020
+
+@author: beck
+"""
+
 import pandas as pd
 import gpxpy
 import pytz
 import dateparser
 import sys
+import datetime
 
-
-def trackExtract(inputPath,gps_filename):
-    """Returns a dataframe containing the telemetry collected from the gpx file"""
-    ext = gps_filename.split('.')
-    if ext[1] == 'csv':
-        gps_telem = pd.read_csv(inputPath+gps_filename)
-        if 'esrignss_latitude' in list(gps_telem.columns):
-            gps_telem = gps_telem.rename(columns={'esrignss_latitude': 'latitude', 'esrignss_longitude': 'longitude','esrignss_altitude':'elevation','esrignss_fixdatetime':'timestamp'})
-            gps_telem1 = pd.DataFrame()
-            gps_telem1['latitude'] = gps_telem['latitude']
-            gps_telem1['longitude'] = gps_telem['longitude']
-            gps_telem1['elevation'] = gps_telem['elevation']
-            gps_telem1['timestamp'] = gps_telem['timestamp']
-            gps_telem = gps_telem1
-        if 'lat' in list(gps_telem.columns):
-            gps_telem = gps_telem.rename(columns={'lat': 'latitude', 'lon': 'longitude','ele':'elevation','time':'timestamp'})
-        i = 0
-        for timestamp in gps_telem['timestamp']:
-            gps_telem.loc[i,'timestamp'] = dateparser.parse(gps_telem.loc[i,'timestamp']).replace(tzinfo=pytz.UTC)
-            i+=1
-    if ext[1] == 'gpx':
-        points = []
-        with open(inputPath+gps_filename,'r') as gpxfile:
-            gpx = gpxpy.parse(gpxfile)
-            for track in gpx.tracks:
-                for segment in track.segments:
-                    sys.stdout.flush()
-                    for point in segment.points:
-                        dict = {'timestamp': point.time,
-                                'latitude': point.latitude,
-                                'longitude': point.longitude,
-                                'elevation': point.elevation
-                                    }
-                        points.append(dict)
+def read_gpx(gps_filename):
+    points = []
+    with open(gps_filename,'r') as gpxfile:
+        gpx = gpxpy.parse(gpxfile)
+        for track in gpx.tracks:
+            for segment in track.segments:
+                sys.stdout.flush()
+                for point in segment.points:
+                    dict = {'timestamp': point.time,
+                            'latitude': point.latitude,
+                            'longitude': point.longitude,
+                            'elevation': point.elevation
+                            }
+                    points.append(dict)
         gps_telem = pd.DataFrame.from_dict(points)
-        i = 0
-        for timestamp in gps_telem['timestamp']:
-            gps_telem.loc[i,'timestamp'] = gps_telem.loc[i,'timestamp'].to_pydatetime().replace(tzinfo=pytz.UTC) 
-            i+=1
     return gps_telem
 
+def trackExtract(gps_filename):
+    ext = gps_filename.split('/')[-1].split('.')
+    if ext[1] == 'csv':
+        gps_telem = pd.read_csv(gps_filename)
+    if ext[1] == 'gpx':
+        gps_telem = read_gpx(gps_filename)
+    return gps_telem
+
+def trackParse(data, time_col, lat_col, long_col, ele_col, track_tz):
+    gps_telem = pd.DataFrame()
+    gps_telem['Timestamp'] = [track_tz.localize(dateparser.parse(data.loc[i,time_col])).astimezone(pytz.timezone('UTC')) for i in range(len(data[time_col]))]
+    print(gps_telem['Timestamp'])
+    gps_telem['Latitude'] = data[lat_col]
+    gps_telem['Longitude'] = data[long_col]
+    if ele_col != 'None':
+        gps_telem['Elevation'] = data[ele_col]
+    return gps_telem
--- vtk.py
+++ vtk.py
@@ -14,12 +14,45 @@ import pandas as pd
 import pytz
 from hachoir.parser import createParser
 from hachoir.metadata import extractMetadata
+from PIL import Image 
+import numpy as np
+import pytesseract
+import imutils
 
+def firstFrame(video):
+    if 'timestamp_frame' not in os.listdir(os.getcwd()):
+        os.mkdir('timestamp_frame/')
+    video_capture = cv2.VideoCapture(video)
+    file = 'timestamp_frame/'+video+'_'+ str(0)+'.jpg'
+    while(True):
+            ret, frame = video_capture.read()
+            if not ret:
+                break
+            im = frame
+            break
+    video_capture.release()
+    PIL_image = Image.fromarray(im.astype('uint8'), 'RGB')
+    return PIL_image
 
-def getCreationDate(filename):
-    parser = createParser(filename)
-    metadata = extractMetadata(parser)
-    return metadata.get('creation_date')
+def formatFrame(image, LEFT = 50, TOP = 20, RIGHT = 250, BOTTOM = 90):
+    image = image.crop((LEFT, TOP, RIGHT, BOTTOM))
+    image = np.array(image.convert('RGB'))[:, :, ::-1].copy()
+    image = imutils.resize(image, width=500)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    return thresh
+
+def getCreationDate(filename, config):
+    if config == 'trident':
+        pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
+        image = formatFrame(firstFrame(video))
+        data = pytesseract.image_to_string(thresh, lang='eng',config='--psm 6')
+        data_str = str(data).split('\n')
+        metadata = dateparser.parse(data_str[0]+ ' '+data_str[1])
+    else:
+        parser = createParser(filename)
+        metadata = extractMetadata(parser).get('creation_date')
+    return metadata
 
 def getOffsets(file):
     #GET DELTA SECONDS FOR EVERY FRAME
@@ -47,43 +80,45 @@ def splitPath(file):
     return folder,filename
         
         
-def getTimestamps(inputPath,file):
-    offsets = getOffsets(inputPath+file)
-    creationdate = getCreationDate(inputPath+file)
+def getTimestamps(file, config):
+    offsets = getOffsets(file)
+    creationdate = getCreationDate(file, config)
     
     #CALCULATE TIMESTAMPS
     timestamps = [creationdate+offset for offset in offsets]
-    timestamps = [timestamp.replace(tzinfo=pytz.UTC) for timestamp in timestamps]
     
     #GENERATE FRAME NAMES
-    frames = [file+'_'+str(i)+'.jpg' for i in range(len(timestamps))]
+    frames = [file.split('/')[-1]+'_'+str(i)+'.jpg' for i in range(len(timestamps))]
     
     #EXPORT DATA AS CSV
     df = pd.DataFrame()
     df['Frame'] = frames
     df['Timestamp'] = timestamps
     return df
-def getFps(inputPath, file):
-    cap = cv2.VideoCapture(inputPath+file)
+def getFps(file):
+    cap = cv2.VideoCapture(file)
     return int(cap.get(cv2.CAP_PROP_FPS))
 
-def createFrames(inputPath,file,projectPath,export_path,taggedDF):
+def createFrames(file,projectPath,export_path,taggedDF):
     taggedList = [taggedDF.loc[i,'Frame'] for i in range(len(taggedDF['Frame']))]
     short_fnames = [int(i.split('.')[1].split('_')[1]) for i in taggedList]
+    frames = {}
     if export_path not in os.listdir(projectPath):
         os.mkdir(projectPath+export_path)
-    cap = cv2.VideoCapture(inputPath+file)
+    cap = cv2.VideoCapture(file)
     sys.stdout.flush()
-    pbar = tqdm(total=len(taggedList), unit='Frames',desc='Writing '+str(len(taggedList))+' frames from '+file + ' to '+export_path)
+    pbar = tqdm(total=len(taggedList))
     i=0
-    while(cap.isOpened()):
-        frame_exists, frame = cap.read()
-        if frame_exists:
-            if i in short_fnames:
-                cv2.imwrite(projectPath+export_path+file+'_'+ str(i)+'.jpg',frame)
-                pbar.update(1)
-            i+=1
-        else:
-            break
-    pbar.close()
+    with tqdm(total=len(taggedList)) as pbar:
+        while(cap.isOpened()):
+            frame_exists, frame = cap.read()
+            if frame_exists:
+                if i in short_fnames:
+                    frames[taggedList[short_fnames.index(i)]] = frame
+                    pbar.update(1)
+                i+=1
+            else:
+                break
     cap.release()
+    for i in tqdm(frames):
+        cv2.imwrite(export_path+'/'+i,frames.get(i))
