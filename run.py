3e5e4588f7482827f3a733e14b4f7c4599191f5a
unknown <becklabash@gmail.com>
2021-02-07 18:41:20 -0500

    check for required packages in run script

--- run.py
+++ run.py
@@ -1,4 +1,41 @@
-from frontend import GeotagTool
+import subprocess
+import time
+import sys
+
+required = {'cv2':'opencv-python',
+            'datetime':'datetime',
+            'dateparser':'dateparser',
+            'pandas': 'pandas',
+            'pytz': 'pytz',
+            'hachoir':'hachoir',
+                'PIL':'Pillow',
+                'numpy':'numpy',
+                'pytesseract':'pytesseract',
+                'imutils':'imutils',
+                'dill':'dill',
+                'gpxpy':'gpxpy',
+                'GPSPhoto':'gpsphoto',
+                'exifread':'exifread',
+                'piexif':'piexif',
+                'threading':'threading'
+                }
+
+for module in required:
+    try:
+        __import__(module)
+    except ImportError as e:
+        print('Missing Package: '+module)
+        print('Attempting to install via pip...')
+        try:
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', required.get(module)])
+        except subprocess.CalledProcessError:
+            print('Failed to install '+ module)
+            print('Exiting in 5 sec...')
+            time.sleep(5)
+            sys.exit()
+        print('Installed '+module)
+print('Starting App')
+from frontend import GeotagTool    
 app = GeotagTool() 
 app.geometry('560x460')
 app.mainloop()
